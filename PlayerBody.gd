af98a86776dbfe1bb8d437dad2115c23eec8d7a6
luluu9 <mnaklicki06@gmail.com>
2019-03-08 09:32:37 +0100

    Add basic collisions

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -1,27 +1,6 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _physics_process(delta):
-#	var force = Vector2(0, -1)
-#	if Input.is_action_pressed("LEFT") and Input.is_action_just_pressed("RIGHT"):
-#		pass
-#	elif Input.is_action_pressed("LEFT"):
-#		force.x = -1
-#		self.rotation -= deg2rad(1)
-#	elif Input.is_action_pressed("RIGHT"):
-#		force.x = 1
-#		self.rotation += deg2rad(1)
-#	move_and_collide(force.rotated(self.rotation)*200*delta)
-
-const MAX_SPEED = 400
+var MAX_SPEED = 400
 const ROT_SPEED = 3.5
 const FRICTION = -0.65
 var THRUST = 600
@@ -42,4 +21,29 @@ func _physics_process(delta):
 	acceleration += velocity * FRICTION
 	velocity += acceleration * delta
 	velocity = velocity.clamped(MAX_SPEED)
-	position += velocity * delta
+	#position += velocity * delta
+	var col = move_and_collide(velocity * delta)
+	if col:
+		var reflect = col.remainder.bounce(col.normal)
+		velocity = velocity.bounce(col.normal)*0.1
+		move_and_collide(reflect)
+
+
+func _on_Body_area_entered(area):
+	print("ENTERED")
+	THRUST = 0
+	rotation *= -1
+	velocity *= -1 * 1
+
+
+func _on_Body_area_exited(area):
+	print("EXITED")
+	THRUST = 600
+
+
+func _on_Thrust_zone_exited(area):
+	MAX_SPEED = 400
+
+
+func _on_Thrust_zone_entered(body):
+	MAX_SPEED = 1000
