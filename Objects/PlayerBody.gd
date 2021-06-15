31e310127608a00d29b46fe0d7adb75dc7f5c06a
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-15 22:22:59 +0200

    Trying new movement system

--- Objects/PlayerBody.gd
+++ Objects/PlayerBody.gd
@@ -10,30 +10,57 @@ var DEFAULT_THRUST = 400
 var velocity = Vector2()
 var acceleration = Vector2()
 
+var wheel_base = 100  
+var engine_power = 800  
+
+var steering_angle = 40
+var steer_angle
+
+var friction = -0.05
+var drag = -0.0015
+
+var slip_speed = 400 
+
+var traction = 0.01  
+
 onready var thrust_ray = get_node("RayCast2D")
 
-func _physics_process(delta):
-	var THRUST = DEFAULT_THRUST
-	if thrust_ray.is_colliding():
-		var col_point = thrust_ray.get_collision_point()
-		var distance = position.distance_to(col_point)
-		THRUST += 10000/distance 
-	print(THRUST)
-	if Input.is_action_pressed("LEFT") and Input.is_action_pressed("RIGHT"):
-		acceleration = Vector2()
-	else:
-		acceleration = Vector2(0, -THRUST).rotated(rotation)
-	if Input.is_action_pressed("LEFT"):
-		rotation -= ROT_SPEED * delta
-	if Input.is_action_pressed("RIGHT"):
-		rotation += ROT_SPEED * delta
 
-	acceleration += velocity * FRICTION
-	acceleration.y += GRAVITY
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("RIGHT"):
+		turn -= 1
+	if Input.is_action_pressed("LEFT"):
+		turn += 1
+	steer_angle = turn * deg2rad(steering_angle)
+	if Input.is_action_pressed("ACCELERATE"):
+		acceleration = transform.x * engine_power
+
+
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	front_wheel += velocity * delta
+	rear_wheel += velocity.rotated(steer_angle) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	velocity = velocity.linear_interpolate(new_heading * velocity.length(), traction)
+	rotation = new_heading.angle()
+
+
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	if velocity.length() < 100:
+		friction_force *= 3
+	acceleration += drag_force + friction_force
+
+
+func _physics_process(delta):
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	calculate_steering(delta)
 	velocity += acceleration * delta
-	# velocity = velocity.clamped(MAX_SPEED)
-	var col = move_and_collide(velocity * delta)
-	if col:
-		var reflect = col.remainder.bounce(col.normal)
-		velocity = velocity.bounce(col.normal)*0.7
-		move_and_collide(reflect)
+	velocity = move_and_slide(velocity)
