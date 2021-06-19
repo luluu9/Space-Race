fae33b59359832fb74d473a61daabc95dbdc997d
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-19 16:40:58 +0200

    Increase the spin thrust on higher velocities

--- Objects/PlayerBody.gd
+++ Objects/PlayerBody.gd
@@ -1,16 +1,18 @@
 extends RigidBody2D
 
 export (int) var engine_thrust = 800 # defines max speed
-export (int) var spin_thrust = 6000
+export (int) var initial_spin_thrust = 6000
 export (int) var side_thrust = 500
 
 var thrust = Vector2()
 var side_vector = Vector2(1, -2)
 var rotation_dir = 0
+var spin_thrust = initial_spin_thrust
 
 onready var thrust_ray = get_node("RayCast2D")
 onready var right_ray = get_node("RayCast2D2")
 onready var left_ray = get_node("RayCast2D3")
+
 
 func get_input():
 	thrust = Vector2()
@@ -26,18 +28,25 @@ func get_input():
 		rotation_dir -= 1
 
 
-func _process(delta):
+func _process(_delta):
 	get_input()
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if thrust_ray.is_colliding():
 		var col_point = thrust_ray.get_collision_point()
 		var distance = position.distance_to(col_point)
 		thrust.x += 50*engine_thrust/distance
 	set_applied_force(thrust.rotated(rotation))
 	set_applied_torque(rotation_dir * spin_thrust)
+	set_spin_thrust()
 	set_side_thrust()
+
+
+func set_spin_thrust():
+	if linear_velocity.length() > engine_thrust/2:
+		spin_thrust = initial_spin_thrust * linear_velocity.length()*2/engine_thrust
+
 
 func set_side_thrust():
 	if left_ray.is_colliding():
