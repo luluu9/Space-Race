3fc8748ee85d7a0acefc3851047ebe865fca04ff
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-27 18:04:25 +0200

    Fix an error about the bubble collision shape

--- Objects/Boosters/Bubble.gd
+++ Objects/Boosters/Bubble.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 func _on_Bubble_body_entered(_body):
 	self.hide()
-	$CollisionShape2D.set_disabled(true)
+	$CollisionShape2D.set_deferred("disabled", true)
 	$Timer.start()
 
 
 func _on_Timer_timeout():
 	self.show()
-	$CollisionShape2D.set_disabled(false)
+	$CollisionShape2D.set_deferred("disabled", false)
