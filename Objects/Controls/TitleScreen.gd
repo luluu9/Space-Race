b397183c3af280d86ec9fa42ec945dd2a0c44770
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-07-04 23:12:06 +0200

    Add a title screen node

--- Objects/Controls/TitleScreen.gd
+++ Objects/Controls/TitleScreen.gd
@@ -1,7 +1,10 @@
 extends Control
  
-signal connect
+signal connect(ip, port)
 signal host
+
+onready var ip_node = $HBoxContainer/VBoxContainer/IpEdit
+onready var port_node = $HBoxContainer/VBoxContainer/PortEdit
 
 
 func _on_HostButton_pressed():
@@ -9,4 +12,13 @@ func _on_HostButton_pressed():
 
 
 func _on_ConnectButton_pressed():
-	emit_signal("connect")
+	var ip = ip_node.text
+	var port = int(port_node.text)
+	if not ip:
+		push_warning("IP address not given")
+		return
+	if not port:
+		push_warning("Port not given")
+		return
+	emit_signal("connect", ip, port)
+	
